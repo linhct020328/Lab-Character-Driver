e454e47f312e02ff443519970b0ecea12d80577c
Thùy linh <39751184+linhct020328@users.noreply.github.com>
2021-04-17 13:05:41 +0700

    Update lab3b1.c

--- lab3_2/lab3b1.c
+++ lab3_2/lab3b1.c
@@ -51,7 +51,60 @@ static void ThayThe(char *XauRo,char *XauMa,char *k){
 	}
 }
 
+//mã hóa hoán vị toàn cục
+static int findMin(char *temp) {
+	int i,j,min,index;
+	min=temp[0];
+	index=0;
+	for (j=0;temp[j]!=NULL;j++) {
+		if(temp[j]<min) {
+			min=temp[j];
+			index=j;
+		}
+	}
+	temp[index]=123;
+	return index;
+}
 
+static int cipher(int i,int r,int l,char arr[22][22], char *XauMa) {
+	int j;
+	for (j=0;j<r;j++) { {
+			XauMa[l++]=arr[j][i];
+		}
+	}
+	return l;
+	//XauMa[k]='\0';
+}
+
+static void HoanViToanCuc(char *XauRo, char *XauMa, char *k){
+	char arr[22][22];
+	char temp[55];
+	int index;
+	strcpy(temp,k);
+	int l=0,flag=0,klen,i,j,row,column;
+	klen = strlen(k);
+	for (i=0; ;i++) {
+		if(flag==1) 
+		    break;
+		for (j=0;k[j]!=NULL;j++) {
+			if(XauRo[l]==NULL) {
+				flag=1;
+				arr[i][j]='-';
+			} else {
+				arr[i][j]=XauRo[l++];
+			}
+		}
+	}
+	row = i;
+	column = j;
+	
+	l=0;
+	for (i=0;i<klen;i++) {
+		index=findMin(temp);
+		l = cipher(index,row,l,arr,XauMa);
+	}
+	XauMa[l]='\0';
+}
 
 //kmalloc: phan bo bo nho cho cac doi tuong nho hon kich thuoc page trong kernel
 //GFP_KERNEL: phan bo RAM binh thuong
@@ -65,6 +118,9 @@ static int __init init_lab(void){
 	char *XauMaThayThe = (char *)kmalloc(15*sizeof(char),GFP_KERNEL);
 	char *k2 = "phqgiumeaylnofdxjkrcvstzwb";
 	
+	char *XauMaHoanViToanCuc = (char *)kmalloc(15*sizeof(char),GFP_KERNEL);
+	char *k3 = "CT2C";
+	
     	printk("\nXau Ro : %s",XauRo);
 	printk(KERN_ALERT "\nThuc hien ma hoa chuyen vi:");
 	ChuyenVi(XauRo,XauMaChuyenVi,k1);	
@@ -73,6 +129,10 @@ static int __init init_lab(void){
 	printk(KERN_ALERT "\nThuc hien ma hoa thay the");
 	ThayThe(XauRo,XauMaThayThe,k2);
 	printk("Xau Ma thay the: %s\n",XauMaThayThe);
+	
+	printk(KERN_ALERT "\nThuc hien ma hoa hoan vi toan cuc");
+	ThayThe(XauRo,XauMaHoanViToanCuc,k3);
+	printk("Xau Ma hoan vi toan cuc: %s\n",XauMaHoanViToanCuc);
 	return 0;
 }
 
